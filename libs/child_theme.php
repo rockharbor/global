144e902a3756a6e692fea3d9b0d13b67f7fe6699
Jeremy Harris <jharris@rockharbor.org>
2012-12-13 11:39:41 -0800

    Loading custom styles

--- libs/child_theme.php
+++ libs/child_theme.php
@@ -22,4 +22,16 @@ class ChildTheme extends RockharborThemeBase {
 		),
 		'hide_name_in_global_nav' => true
 	);
+
+/**
+ * Overrides `setupAssets()` to include some custom assets
+ */
+	function setupAssets() {
+		$base = $this->info('url');
+		wp_register_style('custom', "$base/css/custom.css");
+		
+		wp_enqueue_style('custom');
+		
+		parent::setupAssets();
+	}
 }
