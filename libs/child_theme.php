911d3a5223be9e7654daae4d1266ed60bfd5a0fa
Jeremy Harris <jharris@rockharbor.org>
2013-03-28 09:14:37 -0700

    Moved media queries to css files as method signature changed

--- libs/child_theme.php
+++ libs/child_theme.php
@@ -43,8 +43,8 @@ class ChildTheme extends RockharborThemeBase {
 		$base = $this->info('url');
 		wp_register_script('custom', "$base/js/custom.js");
 		wp_register_style('custom', "$base/css/custom.css");
-		wp_register_style('custom-tablet', "$base/css/custom-tablet.css", array(), false, 'screen and (max-width: 768px)');
-		wp_register_style('custom-mobile', "$base/css/custom-mobile.css", array(), false, 'screen and (max-width: 480px)');
+		wp_register_style('custom-tablet', "$base/css/custom-tablet.css");
+		wp_register_style('custom-mobile', "$base/css/custom-mobile.css");
 
 		wp_enqueue_script('custom');
 		wp_enqueue_style('custom');
