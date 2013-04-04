9f6e8498595be8c38899526a7ce4916e10eba81b
Jeremy Harris <jharris@rockharbor.org>
2013-04-04 13:06:57 -0700

    Pulling service times from correct campus

--- front-page.php
+++ front-page.php
@@ -20,35 +20,35 @@ get_header();
 						<div class="costamesa map"><?php echo $theme->Html->image('map-costamesa.png'); ?></div>
 						<div class="campus-info">
 							<a href="http://costamesa.rockharbor.org" class="campus-link">Costa Mesa</a>
-							<div class="service-times"><?php echo do_shortcode('[service-times campus="1"]'); ?></div>
+							<div class="service-times"><?php echo do_shortcode('[service-times campus="9"]'); ?></div>
 						</div>
 					</li>
 					<li>
 						<div class="mission-viejo map"><?php echo $theme->Html->image('map-missionviejo.png'); ?></div>
 						<div class="campus-info">
 							<a href="http://missionviejo.rockharbor.org" class="campus-link">Mission Viejo</a>
-							<div class="service-times"><?php echo do_shortcode('[service-times campus="1"]'); ?></div>
+							<div class="service-times"><?php echo do_shortcode('[service-times campus="6"]'); ?></div>
 						</div>
 					</li>
 					<li>
 						<div class="fullerton map"><?php echo $theme->Html->image('map-fullerton.png'); ?></div>
 						<div class="campus-info">
 							<a href="http://fullerton.rockharbor.org" class="campus-link">Fullerton</a>
-							<div class="service-times"><?php echo do_shortcode('[service-times campus="1"]'); ?></div>
+							<div class="service-times"><?php echo do_shortcode('[service-times campus="5"]'); ?></div>
 						</div>
 					</li>
 					<li>
 						<div class="huntington-beach map"><?php echo $theme->Html->image('map-huntingtonbeach.png'); ?></div>
 						<div class="campus-info">
 							<a href="http://huntingtonbeach.rockharbor.org" class="campus-link">Huntington Beach</a>
-							<div class="service-times"><?php echo do_shortcode('[service-times campus="1"]'); ?></div>
+							<div class="service-times"><?php echo do_shortcode('[service-times campus="7"]'); ?></div>
 						</div>
 					</li>
 					<li>
 						<div class="orange map"><?php echo $theme->Html->image('map-orange.png'); ?></div>
 						<div class="campus-info">
 							<a href="http://orange.rockharbor.org" class="campus-link">Orange</a>
-							<div class="service-times"><?php echo do_shortcode('[service-times campus="1"]'); ?></div>
+							<div class="service-times"><?php echo do_shortcode('[service-times campus="8"]'); ?></div>
 						</div>
 					</li>
 				</ul>
