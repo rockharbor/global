7d1797fa649e894d7040a7af01c524da11da03b7
Jeremy Harris <jharris@rockharbor.org>
2013-04-24 15:53:23 -0700

    Initializing site

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function _include_theme() {
 	global $theme;
 	require_once 'libs/child_theme.php';
 	$theme = new ChildTheme();
+	$theme->init();
 }
 add_action('after_setup_theme', '_include_theme');
 
